9adaed5fa4ce7437fbf00ffd119437896c7921f9
Aditya Joshi <joshiaditya.aj07@gmail.com>
2023-07-02 17:38:34 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function App() {
+
+  return (
+    <h1 className="font-bold text-6xl text-center text-yellow-500">JAI SHRI RAM</h1>
+     
+  )
+}
+
+export default App
