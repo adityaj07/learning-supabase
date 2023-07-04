77b1111c42dd46943201c2e80151f1f5632d50be
Aditya Joshi <joshiaditya.aj07@gmail.com>
2023-07-05 00:33:10 +0530

    update config

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,8 +1,7 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-
-const supabaseUrl = "https://jhwobqaeeowcxqfurkbr.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impod29icWFlZW93Y3hxZnVya2JyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODgzMDE3ODgsImV4cCI6MjAwMzg3Nzc4OH0.IrnE9Ap6quFc06cfPC5YpWV_-YjQKKDIT7Tblbro-DY"
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
